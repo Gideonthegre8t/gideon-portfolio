c1f82fd844f18067d10bb99c7fba19c105c3cce3
Gideonthegre8t <timilehingabbana502@gmail.com>
2024-04-14 11:34:18 +0100

    Website Upgrade

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
-// import ellipse1 from "../assets/images/Ellipse 1.png";
+import ellipse1 from "../assets/images/Ellipse 1.png";
 import ellipse2 from "../assets/images/Ellipse 2.png";
-// import ellipse from "../assets/images/full-ellipse.png";
-// import profilePhoto from "../assets/images/GIDEON.png";
+import ellipse from "../assets/images/full-ellipse.png";
+import profilePhoto from "../assets/images/GIDEON.png";
 
 function Header() {
   //   //Handle CV download
-  // const handleDownload = () => {
-  // const cvUrl = process.env.PUBLIC_URL + 'https://drive.google.com/file/d/1H-5dAuNiFs3iw-iz_82zkzcfzRaSwBQ9/view?usp=drive_link';
-  // window.open(cvUrl, '_blank')
-  // }
 
   const handleDownloadCV = () => {
     window.open(
@@ -40,14 +36,12 @@ function Header() {
             Download CV
           </button>
         </div>
-        {/* <div className="header-bottom shrink">
+        <div className="header-bottom shrink">
           <img className="ellipse1" src={ellipse1} alt="ellipse-semi-circle" />
           <img className="full-ellipse" src={ellipse} alt="ellipse1" />
-          <img className="photo" src={profilePhoto} alt="profile-pic" />
+          <img className="profile" src={profilePhoto} alt="profile-pic" />
           <img className="ellipse2" src={ellipse2} alt="ellipse2" />
-        </div> */}
-
-         <img  src={ellipse2} alt="profile-pic" />
+        </div>
       </div>
     </header>
   );
